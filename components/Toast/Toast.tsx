051cd46248856d236fd49f5e15809d8602ddd70d
Conrad <36226614+ConradParker@users.noreply.github.com>
2020-11-19 13:15:40 +0100

    Tidy Code

--- components/Toast/Toast.tsx
+++ components/Toast/Toast.tsx
@@ -2,14 +2,14 @@ import React from 'react'
 import styles from './Toast.module.css'
 
 const Toast = (props) => {
-  const { title, message, backgroundColor, icon } = props
+  const { title, message, backgroundColor, icon, onDelete } = props
 
   return (
     <div
       className={`${styles.notification} ${styles.toast}`}
       style={{ backgroundColor: backgroundColor }}
     >
-      <button type='button' className={styles.notificationButton}>
+      <button type='button' className={styles.notificationButton} onClick={() => onDelete()}>
         X
       </button>
       <div className={styles.notificationImageWrapper}>
--- components/Toast/ToastList.tsx
+++ components/Toast/ToastList.tsx
@@ -3,27 +3,47 @@ import Toast from './Toast'
 import styles from './Toast.module.css'
 
 const ToastList = (props) => {
-  const { toastList } = props
+  const { toastList, position } = props
   const [list, setList] = useState(toastList)
 
   useEffect(() => {
     setList(toastList)
   }, [toastList, list])
 
+  const onDelete = (key: Number) => {
+    const index = list.findIndex((e) => e.key === key)
+    list.splice(index, 1)
+    setList([...list])
+  }
+
+  let positionStyle: string
+  switch (position) {
+    case 'topLeft':
+      positionStyle = styles.topLeft
+      break
+    case 'bottomLeft':
+      positionStyle = styles.bottomleft
+      break
+    case 'bottomRight':
+      positionStyle = styles.bottomRight
+      break
+    case 'topRight':
+    default:
+      positionStyle = styles.topRight
+      break
+  }
+
   return (
     <>
-      {/* TODO: Fix dynamic position! <div className={`${styles.notificationContainer} ${position}`}> */}
-      <div
-        className={`${styles.notificationContainer}`}
-        style={{
-          bottom: '12px',
-          right: '12px',
-          transition: 'transform .6s ease-in-out',
-          animation: 'toast-in-right 0.7s'
-        }}
-      >
-        {list.map((toast, i) => (
-          <Toast {...toast} key={i} />
+      <div className={`${styles.notificationContainer} ${positionStyle}`}>
+        {list.map((toast, key) => (
+          <Toast
+            {...toast}
+            key={key}
+            onDelete={() => {
+              onDelete(key)
+            }}
+          />
         ))}
       </div>
     </>
